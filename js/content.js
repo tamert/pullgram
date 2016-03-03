653f2e9094fc709ea7254ef723ef0f1e74c0e5a5
Tamer Ağaoğlu <farerock@gmail.com>
2016-03-03 12:29:09 +0200

    yorum satırları
    console test işlemleri

--- js/content.js
+++ js/content.js
@@ -60,14 +60,49 @@ $(function () {
 
         });
 
-
     }
-
 
 });
 
 
+/*
 
+$( "a[href*='/?tagged=']").each(function() {
+ var link = $(this).attr('href')+'&__a=1';
+  $.getJSON( link, function( data ) {
+    console.log(data);
+  });
+});
+
+var link = $( "a[href*='/?tagged=']").eq(0).attr('href')+'&__a=1';
+$.getJSON( link, function( data ) {
+    console.log(data);
+});
+
+function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+ig_shortcodes = 'ig_shortcode(BCeKpqWOsRp)+%7B%0A++comments.before(%0A++++++++++1197635690653467744%2C%0A++++++++++99%0A++++++++)+%7B%0A++++count%2C%0A++++nodes+%7B%0A++++++id%2C%0A++++++created_at%2C%0A++++++text%2C%0A++++++user+%7B%0A++++++++id%2C%0A++++++++profile_pic_url%2C%0A++++++++username%0A++++++%7D%0A++++%7D%2C%0A++++page_info%0A++%7D%0A%7D%0A';
+
+
+ig_shortcodes = replaceAll(ig_shortcodes, '+', ' ');
+
+console.log(ig_shortcodes);
+
+//(
+
+//ig_shortcode    =  { 'ig_shortcode(BCeKpqWOsRp) { comments.before( 1197638113811940560, 50 ) { count, nodes { id, created_at, text, user { id, profile_pic_url, username } }, page_info } }' };
+
+$.post('/query', { q: decodeURIComponent(ig_shortcodes), ref: 'media::show' }, function(response) {
+    // Do something with the request
+}, 'json');
+
+*/
 
 
 
